ca9895d4d3eabb8582144cf9679bc577fb33eb60
Martinmessner <martinmessner95@gmail.com>
2024-03-25 22:41:39 -0300

    add graph

---
+++ src/app/components/GraficChart.tsx
@@ -0,0 +1,57 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export default function GraphicTotal({
+  contenedorTotal,
+}: {
+  contenedorTotal: {
+    [x: string]: { date: string; value: string; description: string };
+  };
+}) {
+  const GraphicLabels = Object.keys(contenedorTotal);
+  const GraphicValues = Object.values(contenedorTotal).map(
+    (data) => data.value
+  );
+
+  const data = {
+    labels: GraphicLabels,
+    datasets: [
+      {
+        label: "Valor",
+        data: GraphicValues,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return <Doughnut data={data} />;
+}
